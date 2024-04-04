cd3a677eb08f8167e84cd272b0947979973ce06e
Artem Naboka <83280960+SkyFoxNa@users.noreply.github.com>
2024-04-04 19:51:18 +0300

    Update config.py

--- src/conf/config.py
+++ src/conf/config.py
@@ -10,7 +10,5 @@ DB_NAME = os.getenv("DB_NAME")
 
 class Config:
     DB_URL = f"postgresql+asyncpg://{DB_USERNAME}:{DB_PASSWORD}@trumpet.db.elephantsql.com/{DB_NAME}"
-    # DB_URL = "postgresql+asyncpg://pjpggqwg:g0oERr5tmQJ1emqQ9jUqddgPeo2CfoHV@trumpet.db.elephantsql.com/pjpggqwg"
-
 
 config = Config
